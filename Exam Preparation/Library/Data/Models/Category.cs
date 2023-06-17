fb7af0ce81b1db6502fa82d395881c92ba4c4b4e
Niicksan <nikslotar@abv.bg>
2023-06-17 13:56:19 +0300

    Exam Preparation - Library

---
+++ Exam Preparation/Library/Data/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Models
+{
+    [Comment("Categoryies for the book")]
+    public class Category
+    {
+        [Comment("Primery Key")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of the Category")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ Exam Preparation/Library/Iterfaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Library.Models;
+
+namespace Library.Iterfaces
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync();
+        Task AddBookAsync(AddBookViewModel model);
+        Task EditBookAsync(AddBookViewModel model, int id);
+        Task<BookViewModel?> GetBookByIdAsync(int id);
+        Task<AddBookViewModel?> GetBookByIdForEditAsync(int id);
+        Task AddBookToCollectionAsync(string userId, BookViewModel book);
+        Task<IEnumerable<AllBookViewModel>> GetMyBooksAsync(string userId);
+        Task<AddBookViewModel> GetNewAddBookModelAsync();
+        Task RemoveBookFromCollectionAsync(string userId, BookViewModel book);
+    }
+}
